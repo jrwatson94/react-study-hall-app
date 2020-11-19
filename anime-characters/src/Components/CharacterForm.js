0f8e699688ea76b6a531f100c4389275583bf1b2
jrwatson94 <j.watsonreid@gmail.com>
2020-11-19 14:07:23 -0500

    form refactor

--- anime-characters/src/Components/CharacterForm.js
+++ anime-characters/src/Components/CharacterForm.js
@@ -9,28 +9,29 @@ export default class CharacterForm extends React.Component {
     submit = (event) => {
         event.preventDefault()
         console.log("working")
-        console.log(document.getElementById("name").value)
-        this.setState({
-            name: document.getElementById("name").value,
-            anime: document.getElementById("anime").value,
-            img: document.getElementById("image").value
-        })
         this.props.newCharacter(this.state)
+    }
+
+    changeHandler = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+        console.log(this.state)
     }
     render(){
         return(
             <form onSubmit={this.submit}>
                 <h2>Create New Character</h2>
                 <label>Name:</label>
-                <input type="text" id="name"></input>
+                <input type="text" name="name" value={this.state.name} onChange={this.changeHandler}></input>
                 <br></br>
 
                 <label>Anime:</label>
-                <input type="text" id="anime"></input>
+                <input type="text" name="anime" value={this.state.anime} onChange={this.changeHandler}></input>
                 <br></br>
 
                 <label>Image Link:</label>
-                <input type="text" id="image"></input>
+                <input type="text" name="img" value={this.state.img} onChange={this.changeHandler}></input>
                 <div>
                     <button type="submit">Create</button>
                 </div>
